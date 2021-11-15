53f467f831c867d4d99207ab01812dc0f9c3fb8a
Karthik <wrk.krthik@gmail.com>
2021-11-15 18:50:56 +0530

    commit to main

---
+++ include/gpio.h
@@ -0,0 +1,28 @@
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifndef __AVR_ATmega328P__
+    #define __AVR_ATmega328P__
+#endif
+
+#include <avr/io.h>
+
+#define PIN_SET 1
+#define PIN_RESET 0
+
+#define GPIO_PORT_B 0b001
+#define GPIO_PORT_C 0b010
+#define GPIO_PORT_D 0b011
+
+void GPIO_Write(uint8_t GPIO_Port,uint8_t GPIO_Pin,uint8_t PIN_State);
+uint8_t GPIO_Read(uint8_t GPIO_Port,uint8_t GPIO_Pin);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
