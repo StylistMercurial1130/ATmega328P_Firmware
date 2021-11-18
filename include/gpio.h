7be77e4264d3d357639130d24e2a031fd2ac2feb
Karthik <wrk.krthik@gmail.com>
2021-11-18 14:47:19 +0530

    commit to main

--- include/gpio.h
+++ include/gpio.h
@@ -17,16 +17,9 @@ extern "C"{
 #define PIN_OUPUT 1
 #define PIN_INPUT 0
 
-#define GPIO_PORT_B 0b001
-#define GPIO_PORT_C 0b010
-#define GPIO_PORT_D 0b011
+void GPIO_Write(volatile uint8_t * GPIO_Port,uint8_t GPIO_Pin,uint8_t PIN_State);
+uint8_t GPIO_Read(volatile uint8_t * GPIO_Port,uint8_t GPIO_Pin);
+void GPIO_Set_Pins(volatile uint8_t * GPIO_Port,uint8_t GPIO_Pin,uint8_t State);
 
-void GPIO_Write(uint8_t GPIO_Port,uint8_t GPIO_Pin,uint8_t PIN_State);
-uint8_t GPIO_Read(uint8_t GPIO_Port,uint8_t GPIO_Pin);
-void GPIO_Set_Pins(uint8_t GPIO_Port,uint8_t GPIO_Pin,uint8_t State);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
