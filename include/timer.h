f70c64c771d9fe1bda61d6bb784c5d57dd557085
Karthik <wrk.krthik@gmail.com>
2021-12-01 10:39:33 +0530

    commit to main

--- include/timer.h
+++ include/timer.h
@@ -5,39 +5,23 @@
  *
  */
 
-#ifdef __cplusplus
-	extern "C" {
-#endif
-
 #ifndef __TIMER_H__
-	#define __TIMER_H__ 
+#define __TIMER_H__ 
 
-#ifndef __ATMega328P__
-	#define __ATMega328P__
+#ifndef __AVR_ATmega328P__
+	#define __AVR_ATmega328P__
 #endif
 
 #include <avr/io.h>
 
-/*
- *	TODO ! Set COMO0A1 and COM0A0 registers based on input configuration 
- */
+#define COM_MODE_NORMAL 0b00
+#define COM_MODE_TOGGLE 0b01
+#define COM_MODE_CLEAR 0b10
+#define COM_MODE_SET 0b11
 
-typedef enum {
-	
-	NORMAL = 0b00 , 
-	TOGGLE = 0b01 ,
-	CLEAR = 0b10 ,
-	SET = 0b11 , 
-
-} Compare_Output_Mode
-
-typedef struct TIMER_0 Timer_0;
-typedef struct TIMER_1 Timer_1;
-typedef struct TIMER_2 Timer_2;
+void Set_COM0A_COM_Mode(uint8_t Mode);
+void Set_COM0B_COM_Modes(uint8_t Mode);
 
 
 #endif
 
-#define __cplusplus
-	}
-#endif
